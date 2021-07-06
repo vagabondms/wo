38bf56f00cc0ae46c4329cf2dc3df9e225586564
min <min@mins-MacBook-Pro.local>
2021-07-06 21:38:27 +0900

    :art: shareProgram 구현

--- src/server/controllers/program.ts
+++ src/server/controllers/program.ts
@@ -131,15 +131,6 @@ export const deleteProgram = async (
   }
 };
 
-/* 
-  프로그램 공유에 대해 고민해봐야할 것.
-  1. owner만 공유/공유 해제를 할 수 있는데, 이때 만약 누군가 program을 가져가면 그때는 어떻게 작동시키는지?
-  => 두가지 경우가 있을 것 같은데, 
-        1) owner 바꾸기, 
-        2) 공유 자체를 owner 한테 가서만 할 수 있게함. 따라서 가져간 사용자들은 공유/비공유에 영향을 끼칠 수 없게 제한.
-    현재는 후자가 좋아 보이는데 생각을 더 해봐야 할 듯.
- */
-
 // * 프로그램 공유 설정
 export const shareProgram = async (
   req: Request,
@@ -147,28 +138,29 @@ export const shareProgram = async (
   next: NextFunction,
 ): Promise<void> => {
   try {
-    // const programId: Type.Id = req.params.programId;
-    // const userId: Type.Id = 27; //TODO: session 값으로 교체
+    const programId: Type.Id = req.params.programId;
+    const userId: Type.Id = 1; //TODO: session 값으로 교체
 
-    // const {
-    //   raw: { changedRows },
-    // } = await getRepository(Program)
-    //   .createQueryBuilder()
-    //   .update()
-    //   .set({
-    //     isShared: true,
-    //   })
-    //   .where('id=:programId', { programId: 1 })
-    //   .andWhere('ownerId = :userId', { userId })
-    //   .execute();
+    const {
+      affected,
+      raw: { changedRows },
+    } = await getConnection()
+      .createQueryBuilder()
+      .update(Program_User)
+      .set({ isShared: true })
+      .where('programId = :programId', { programId })
+      .andWhere('userId = :userId', { userId })
+      .execute();
 
-    // //* 변경된 것이 없으면 잘못된 요청임.
-    // if (changedRows === 0) {
-    //   res.status(403).json('잘못된 요청입니다.');
-    //   return;
-    // }
-
-    // res.status(200).send(programId);
+    if (affected === 0) {
+      res.status(400).send('해당 프로그램을 갖고 있지 않습니다.');
+      return;
+    }
+    if (changedRows === 0) {
+      res.status(400).send('이미 공유되고 있습니다.');
+      return;
+    }
+    res.status(200).send(programId);
     return;
   } catch (err) {
     console.error(err);
