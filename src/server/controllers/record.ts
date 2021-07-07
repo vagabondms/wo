7008b7c9c8ea1f01ce923fd5f7f9cb5e0a20586e
vagabondms <hiseokseok@gmail.com>
2021-07-07 13:41:32 +0900

    :sparkles: getRecord 구현

--- src/server/controllers/record.ts
+++ src/server/controllers/record.ts
@@ -1,8 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
+import { getRepository } from 'typeorm';
+
+import * as Type from 'types/post';
+import Record from '../entity/Record.entity';
 
 // * record 보기
-export const getRecord = (req: Request, res: Response, next: NextFunction): void => {
+export const getRecord = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
+    const userId: Type.Id = 1; //TODO: 추후 session에서 꺼내쓰는 것으로 대체
+    const exerciseId: Type.Id = req.params.exerciseId;
+
+    const records: Record[] = await getRepository(Record)
+      .createQueryBuilder()
+      .where('userId = :userId', { userId })
+      .andWhere('exerciseId = :exerciseId', { exerciseId })
+      .getMany();
+
+    res.status(200).send(records);
+
     return;
   } catch (err) {
     console.error(err);
@@ -12,7 +27,11 @@ export const getRecord = (req: Request, res: Response, next: NextFunction): void
 };
 
 // * record 생성
-export const createRecord = (req: Request, res: Response, next: NextFunction): void => {
+export const createRecord = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
   try {
     return;
   } catch (err) {
@@ -23,7 +42,11 @@ export const createRecord = (req: Request, res: Response, next: NextFunction): v
 };
 
 // * record 삭제
-export const deleteRecord = (req: Request, res: Response, next: NextFunction): void => {
+export const deleteRecord = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
   try {
     return;
   } catch (err) {
--- src/server/routes/record.ts
+++ src/server/routes/record.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import { getRecord, createRecord, deleteRecord } from '../controllers/record';
 
 const router = express.Router();
+
+router.get('/:exerciseId', getRecord);
+router.post('/', createRecord);
+router.delete('/', deleteRecord);
 
 export default router;
