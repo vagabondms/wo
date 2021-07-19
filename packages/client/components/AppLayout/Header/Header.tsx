c5e0d986f89c0bc118207f935e9d34553586feb1
vagabondms <hiseokseok@gmail.com>
2021-07-19 20:57:07 +0900

    :construction: header 작업중

--- packages/client/components/AppLayout/Header/Header.tsx
+++ packages/client/components/AppLayout/Header/Header.tsx
@@ -2,25 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 
 import User from '@icons/user.svg';
+import SubHeader from '../SubHeader/SubHeader';
 
 const HeaderDiv = styled.div`
   grid-area: head;
-  height: 120px;
+  height: 144px;
   background: skyblue;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-right: 47px;
+  padding-left: 90px;
+  font-weight: bold;
+  font-size: 20px;
+  color: #26207e;
 `;
 
-const CustomUser = styled(User)`
-  height: 10px;
-  float: right;
-  color: blue;
-  border: 1px black;
+const ProfileWrapper = styled.div`
+  display: flex;
+  width: 220px;
+  align-items: center;
+  justify-content: space-between;
+  font-weight: normal;
+  font-size: 16px;
+  color: #949cff;
 `;
 
 const Header = () => {
   return (
     <HeaderDiv>
-      <CustomUser />
-      Header
+      공유
+      <ProfileWrapper>
+        안녕하세요. 김민석님!
+        <User />
+      </ProfileWrapper>
+      <SubHeader />
     </HeaderDiv>
   );
 };
